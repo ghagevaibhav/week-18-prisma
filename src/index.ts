d8de0db0ddaede3534fde9f2946015d5293ed2d5
ghagevaibhav <ghagevaibhav@gmail.com>
2024-12-08 09:44:55 +0530

    expressify prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
+import express from 'express'
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/users', async (req: any, res: any) => {
+    const result = await prisma.user.findMany();
+    return res.json(result)
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+})
